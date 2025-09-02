9ce224f7ffc6c289a8b1c1d5d6557fe44cc3754c
steven.saldana[200~@ <steven.saldana@tecsup.edu.pe>
2025-09-01 22:48:25 -0500

    Student Repository

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,4 +1,32 @@
 package pe.edu.tecsup.lab03.repositories;
 
+import pe.edu.tecsup.lab03.entity.StudentEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentRepository {
+
+    private List<StudentEntity> students;
+
+    public StudentRepository() {
+        this.students = new ArrayList<>();
+    }
+
+    public void save(StudentEntity student) {
+        students.add(student);
+    }
+
+    public List<StudentEntity> findAll() {
+        return students;
+    }
+
+    public StudentEntity findById(String id) {
+        for (StudentEntity student : students) {
+            if (student.getId().equals(id)) {
+                return student;
+            }
+        }
+        return null;
+    }
 }
